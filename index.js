6642ca6d18e0c7f8c6f4fd713745858ec9ad4ba6
me-mega-noob-4044 <testtrolium@gmail.com>
2024-10-31 12:37:12 -0400

    starting homepage

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("*", (req, res) => {
-    res.send("HI :)");
+    const { url } = req;
+    if (url.includes("favicon.ico")) {
+        res.sendFile(path.join(__dirname, "/homepage/favicon.ico"));
+    } else {
+        if (url) {
+            res.sendFile(path.join(`${__dirname}/homepage/${url}`));
+        } else {
+            res.sendFile(path.join(__dirname + "/homepage/index.html"));
+        }
+    }
 })
 
 app.listen(3030, () => {
