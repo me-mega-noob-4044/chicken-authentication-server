6124306fee7f7a90eb86ede0c3d67bc703cfc127
me-mega-noob-4044 <testtrolium@gmail.com>
2024-11-06 16:52:05 -0500

    login background

--- login/script.js
+++ login/script.js
@@ -100,3 +100,26 @@ if (localStorage.cached || devauled) {
 document.getElementById("home").onclick = () => {
     location.href = "/";
 };
+
+var images = ["../assets/bg-image-1.png", "../assets/bg-image-2.png", "../assets/bg-image-3.png", "../assets/bg-image-4.png"];
+var bgImage = document.getElementById("bg-image");
+
+var indx = 0;
+function changeBG() {
+    bgImage.classList.add("blink");
+
+    setTimeout(() => {
+        bgImage.style.backgroundImage = `url("${images[indx]}")`;
+        indx++;
+
+        if (indx >= images.length) {
+            indx = 0;
+        }
+    }, 350);
+
+    setTimeout(() => {
+        bgImage.classList.remove("blink");
+    }, 800);
+}
+
+setInterval(changeBG, 10e3);
